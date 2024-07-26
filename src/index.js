9663e4d29cddef5cb5083d9f80954397437db09a
swoo0514 <swoo0514lim@gmail.com>
2024-07-26 21:24:35 +0900

    index.js 초기 세팅

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-require('dotenv').config();
-
 const express = require('express');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+dotenv.config();
+
 const app = express();
+app.set('port', process.env.PORT || 8000);
+
+if (process.env.NODE_ENV === 'production') {
+  app.enable('trust proxy');
+  app.use(morgan('combined'));
+  app.use(
+    helmet({
+      contentSecurityPolicy: false,
+      crossOriginEmbedderPolicy: false,
+      crossOriginResourcePolicy: false,
+    })
+  );
+} else {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기중');
+});
